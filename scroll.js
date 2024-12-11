bb46f15ef2d8ac3c820ebef15ec16029fe24a070
Marcus Makhubela <104652163+Marcusdev91@users.noreply.github.com>
2024-12-11 10:39:26 -0800

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,36 @@
+setTimeout(function() { $('#loader').fadeOut('slow', function() { $('#content').fadeIn('slow'); }); }, 5000);
+
+$(document).ready(function() {
+    //loader
+    
+    
+    // Function to check if an element is in viewport
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Function to add the 'visible' class to elements in viewport
+    function revealOnScroll() {
+        $('.hidden').each(function() {
+        
+            $(window).on('scroll', function() { $('.hidden').each(function() { var elementTop = $(this).offset().top; var viewportBottom = $(window).scrollTop() + $(window).height(); if (elementTop < viewportBottom) { $(this).addClass('visible'); } }); });
+        });
+    }
+
+    // Run the reveal function on scroll
+    $(window).on('scroll', revealOnScroll);
+
+    // Run the reveal function on page load
+    revealOnScroll();
+
+ 
+
+});
+
+
